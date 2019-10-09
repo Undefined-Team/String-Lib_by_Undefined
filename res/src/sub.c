f1a5ac543e93021ff8757a268d1cde6f8399ff20
sbedene <sylvainbedene@gmail.com>
2019-10-09 18:17:32 +0200

    bug in strsub

--- res/src/sub.c
+++ res/src/sub.c
@@ -6,7 +6,8 @@ char    *ud_str_sub(char *str, size_t start, size_t len)
 
     if (!str) return NULL;
     UD_UT_PROT_MALLOC(sub = ud_ut_malloc(sizeof(char) * (len + 1)));
-    char *p_sub = &sub[start];
+    char *p_sub = sub;
+    str = &str[start];
     for (ud_ut_count i = 0; i < len && *str; ++i, ++p_sub, ++str)
         *p_sub = *str;
     return (sub);
