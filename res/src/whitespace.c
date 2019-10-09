d6dec3dbbbf6bf75bc86541184c7fe0d7124dc96
sbedene <sylvainbedene@gmail.com>
2019-10-09 15:11:27 +0200

    whitespace

--- res/src/whitespace.c
+++ res/src/whitespace.c
@@ -5,12 +5,8 @@ char    *ud_str_whitespace(char *str)
     ud_ut_count     i = 0;
     ud_ut_count     j = ud_str_len(str) - 1;
 
-    char *a_str = (char*)str->val;
-    if (!a_str || str->len == 0)
-        return (str);
-    while (ud_str_is_white_space(a_str[i]))
-        i++;
-    while (ud_str_is_white_space(a_str[j]))
-        j--;
-    return (ud_str_sub(str, i, j - i + 1));
+    if (!str || !str[0]) return (str);
+    while (ud_str_is_white_space(str[i])) ++i;
+    while (ud_str_is_white_space(str[j])) --j;
+    return ud_str_sub(str, i, j - i + 1);
 }
