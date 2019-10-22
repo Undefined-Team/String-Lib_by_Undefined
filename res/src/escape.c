a72dafa56c86d3a93009d742fa1373f8f51d7991
sbedene <sylvainbedene@gmail.com>
2019-10-22 20:36:05 +0200

    ud str fescape

--- res/src/escape.c
+++ res/src/escape.c
@@ -1,12 +1,14 @@
 #include "ud_string.h"
 
-char    *ud_str_escape(char *str)
+char    *ud_str_escape_ctr(char *str, ud_bool need_free)
 {
     int     i = 0;
     int     j = ud_str_len(str) - 1;
 
     if (!str || !str[0]) return (str);
-    if (str[i] == '\"' && str[j] == '\"') return ud_str_sub(str, i + 1, j - 1);
-    if (str[i] == '\'' && str[j] == '\'') return ud_str_sub(str, i + 1, j - 1);
-    return (str);
+    char *ret = str;
+    if (str[i] == '\"' && str[j] == '\"') ret = ud_str_sub(str, i + 1, j - 1);
+    if (str[i] == '\'' && str[j] == '\'') ret = ud_str_sub(str, i + 1, j - 1);
+    if (need_free && ret != str) ud_ut_free(str);
+    return (ret);
 }
